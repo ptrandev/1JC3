129d5396ccbd643f494a6d058bafac61200d5cad
Phillip Tran <ptrandev@protonmail.com>
2025-03-03 09:31:37 -0500

    tutorial 07

--- tutorial07/tutorial07.hs
+++ tutorial07/tutorial07.hs
@@ -11,7 +11,6 @@
 -- May help you understand how to test programs and prove properties in the
 -- future
 
-
 -- cabal install --lib --package-env . QuickCheck
 import Test.QuickCheck
 
@@ -24,22 +23,18 @@ import Test.QuickCheck
 -- 2. Do the same by hand, with line-by-line calculation
 -- 3. Or we can reason about how the program behaves in general
 
--- consider a simple function definition by pattern matching
-
 sum' :: Num a => [a] -> a
-sum' [] = 0 -- def sum.1
-sum' (x:xs) = x + sum' xs -- def sum.2
-
--- sum [2,3,2] = 7
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
 
 {-
-  sum [2,3,2]
-  = sum (2:(3:(2:[])))
-  = 2 + sum (3:(2:[]))
-  = 2 + 3 + sum (2:[])
-  = 2 + 3 + 2 + sum []
-  = 2 + 3 + 2 + 0
-  = 7
+sum [2,3,2]
+= sum(2:(3:(2:[])))
+= 2 + sum(3:(2:[]))
+= 2 + 3 + sum (2:[])
+= 2 + 3 + 2 + sum []
+= 2 + 3 + 2 + 0
+= 7
 -}
 
 -- reason about how the function should behave by defining a property that
@@ -51,36 +46,34 @@ sumOneProp x = sum [x] == x
 -- 2. Proving Properties
 --
 
--- let us prove a certain property by reasoning about case definitions
+-- sum' :: Num a => [a] -> a
+-- sum' [] = 0 -- def. of sum.1
+-- sum' (x:xs) = x + sum' xs -- def. of sum.2
 
 {-
-sum [x]
-= sum (x:[]) -- by def. of a list
-= x + sum [] -- by def. of sum.2
-= x + 0 -- by def. of sum.1
-= x -- integer arithmetic
+  sum [x]
+  = sum (x:[]) -- by def. of a list
+  = x + sum [] -- by def. of sum.2
+  = x + 0 -- by def. of sum.1
+  = x -- def. of identity
 -}
 
 --
 -- 3. Definedness and Termination
 --
 
--- Termination = the function will stop running
--- Definedness = the function will return a result for the given input
+-- Termination = The function will stop running
+-- Definedness = The function returns a result for a given input
 
 -- consider the function for computing factorials
 fact :: Integer -> Integer
 fact 0 = 1
 fact n = n * fact (n-1)
 
--- try entering...
--- fact (-1) -- -1 is NOT DEFINED for fact and will not terminate
+-- fact is not defined for < 0
 
-mod :: Integral a => a -> a -> a
-mod x y = x - y * div x y
-
--- try entering...
--- mod 5 0 -- 0 is NOT DEFINED for mod
+-- mod :: Integral a => a -> a -> a
+-- mod x y = x - y * div x y
 
 -- so when you're proving these algorithms, there may be some inputs that are
 -- not defined for the function, and the function may not terminate for some
@@ -103,15 +96,18 @@ mod x y = x - y * div x y
 --   (True, False) -> False
 --   _ -> True
 
--- a === b = (not a) || b
-
--- Prepositional Logic
+-- a b o
+-- T F F
+-- T T T
+-- F T T
+-- F F T
 
 and :: Bool -> Bool -> Bool
 and True True = True
 and _ _ = False
 
 -- (The sky is blue) and (the grass is orange)
+-- True and False
 -- False
 
 or :: Bool -> Bool -> Bool
@@ -119,6 +115,7 @@ or False False = False
 or _ _ = True
 
 -- (The sky is blue) or (the grass is orange)
+-- True or False
 -- True
 
 not :: Bool -> Bool
@@ -126,22 +123,26 @@ not True = False
 not False = True
 
 -- not (The sky is blue)
+-- not (True)
 -- False
 
 implies :: Bool -> Bool -> Bool
 implies True False = False
 implies _ _ = True
 
--- if (The sky is blue) then (the grass is green)
 -- (The sky is blue) implies (the grass is green)
+-- True implies True
 -- True
 
 -- (The sky is orange) implies (the grass is green)
+-- False implies True
 -- True
+
 -- Why? The statement only becomes False when the first part is True and the
 -- second part is False; otherwise it is ALWAYS True
 
 -- (The sky is blue) implies (the grass is orange)
+-- True implies False
 -- False
 
 -- Why is this useful? We use it to prove properties mathematically
@@ -154,57 +155,64 @@ implies _ _ = True
 -- fact 0 = 1
 -- fact n = n * fact (n-1)
 
-
--- the following QuickCheck property for fact
 factProp :: Integer -> Bool
 factProp n = fact n `div` n == fact (n-1)
 
--- we get a divide by zero error
--- quickCheck factProp
+-- fact 3 `div` 3 == fact 2
+-- 3 * 2 * 1 = 6 / 3 = 2
+-- 2 * 1 = 2
 
--- so we gotta modify the property to exclude the case where n = 0
 factProp' :: Integer -> Bool
 factProp' n = if n == 0 then True else fact n `div` n == fact (n-1)
-
--- we get a stack overflow... why?
--- we have a negative number as a test case, we need to exclude that as well
 
 factProp'' :: Integer -> Bool
 factProp'' n = if n <= 0 then True else fact n `div` n == fact (n-1)
 
--- now it passes!
+-- a ==> b = case (a,b) of
+--   (True, False) -> False
+--   _ -> True
 
--- we can also use the implication operator to exclude certain cases
+-- a b o
+-- T F F
+-- T T T
+-- F T T
+-- F F T
+
 factProp''' :: Integer -> Property
-factProp''' n = n >= 0 ==> fact n `div` n == fact (n-1)
+factProp''' n = n > 0 ==> fact n `div` n == fact (n-1)
 
--- Proving factprop
-
--- fact :: Integer -> Integer
--- fact 0 = 1 -- def fact.1
--- fact n = n * fact (n-1) -- def fact.2
+fact :: Integer -> Integer
+fact 0 = 1 -- def fact.1
+fact n = n * fact (n-1) -- def fact.2
 
 {-
-Proof (n >= 1):
+Proof: fact n `div` n == fact (n-1) for n > 0
 fact n `div` n
-= (n * fact (n-1)) `div` n -- by def. of fact.2
-= fact (n-1) -- by integer arithmetic, since n / n
+= n * fact (n-1) `div` n -- by def. of fact.2
+= fact (n-1) -- by rules of division
 -}
 
 --
 -- 6. Structural Induction on Lists
 --
 
--- Prove that a logical property P(xs) hold for all finite lists xs, we must...
--- Base Case (xs = []): Prove that P([]) holds
--- Inductive Case (xs = x:xs'): Prove that if P(xs') holds, then P(x:xs') holds
+-- Prove that a logical property P(xs) hold for all finite lists of xs
+-- 1. Base Case (xs = []): P([]) holds
+-- 2. Inductive Case (xs = x:xs') Prove if P(xs') holds, then P(x:xs') holds
 
 -- P(xs) is known as the induction hypothesis
--- i.e. we assume it to be true and prove P(x:xs)
+-- i.e. we assume it to be true and prove P(x:xs')
+
+-- xs = [2,3,4]
+-- x = [2]
+-- xs' [3,4]
+
+-- P([3,4])
+-- P(2 : [3,4])
 
 reverse' :: [a] -> [a]
-reverse' [] = []
-reverse' (x:xs) = reverse' xs ++ [x]
+reverse' [] = [] -- def. of reverse.1
+reverse' (x:xs) = reverse' xs ++ [x] -- def. of reverse.2
 
 -- Reasoning about correctness using QuickCheck
 reverseProp :: [Integer] -> Bool
@@ -213,38 +221,45 @@ reverseProp x = reverse [x] == [x]
 {-
 Proof
 reverse [x]
-= reverse (x:[]) -- by def. of list
+= reverse (x:[]) -- by def. of a list
 = reverse [] ++ [x] -- by def. of reverse.2
 = [] ++ [x] -- by def. of reverse.1
 = [x] -- by list arithmetic
 -}
 
+reverse' :: [a] -> [a]
+reverse' [] = [] -- def. of reverse.1
+reverse' (x:xs) = reverse' xs ++ [x] -- def. of reverse.2
+
 reverseProp2 :: [Integer] -> Bool
 reverseProp2 xs = reverse (reverse xs) == xs
 
 {-
+Lemma (assume this to be true for the inductive step):
+reverse (xs ++ ys) = reverse ys ++ reverse xs
+
+P(x:xs) = True
+P(xs) = True
+
+Inductive Step:
+reverse (reverse(x:xs))
+= reverse (reverse xs ++ [x]) -- by def. of reverse.2
+= reverse [x] ++ reverse (reverse xs) -- by lemma
+= [x] ++ reverse (reverse xs) -- by prev proof
+= [x] ++ xs -- by inductive hypothesis
+= x:xs -- by list arithmetic
+
 Base Case:
 reverse (reverse [])
 = reverse [] -- by def. of reverse.1
 = [] -- by def. of reverse.1
-
-Lemma (assume this to be true for the inductive step):
-reverse (xs ++ ys) = reverse ys ++ reverse xs
-
--- Inductive Step:
-reverse (reverse (x:xs))
-= reverse (reverse xs ++ [x]) -- by def. of reverse.2
-= reverse [x] ++ reverse (reverse xs) -- by lemma
-= [x] ++ reverse (reverse xs) -- by prev proof
-= [x] ++ xs -- by induction hypothesis
-= x:xs -- by list arithmetic
 -}
 
 -- Another example
 
 length :: [a] -> Int
-length [] = 0
-length (_:xs) = 1 + length xs
+length [] = 0 -- def length.1
+length (_:xs) = 1 + length xs -- def length.2
 
 -- takes an integer n and returns the first n elements of a list
 take' :: Int -> [a] -> [a]
@@ -256,20 +271,21 @@ take' n (x:xs)
 takeLengthProp :: [a] -> Bool
 takeLengthProp xs = take (length xs) xs == xs
 
--- can we prove this property of both take and length? Yes, using structural induction
-
 {-
-Base Case:
-take (length []) []
-= take 0 [] -- by def. of length.1
-= [] -- by def. of take.1
-
 Inductive Step:
-take (length (x:xs)) (x:xs)
+take (length (x:xs)) (x:xs) == x:xs
 = take (1 + length xs) (x:xs) -- by def. of length.2
-= x : take (1 + length xs - 1) xs -- by def. of take.2
+= x : take (1 + length xs - 1) (xs) -- by def. of take.2
 = x : take (length xs) xs -- by integer arithmetic
 = x : xs -- by induction hypothesis
+
+Base Case:
+take (length []) []
+= take 0 [] -- by definition of length.1
+= [] -- by definition of take.1
+
+take (length xs) xs == xs
+take (length x:xs) x:xs == x:xs
 -}
 
 -- Tips
